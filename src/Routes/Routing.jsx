d09535bedb0578bac369b08cdbc0497a878ca8c8
Anas Raza <anus.raza987@gmail.com>
2021-12-16 12:44:57 +0500

    minor changes

--- src/Routes/Routing.jsx
+++ src/Routes/Routing.jsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // import About from '../Components/About';
-import Error from '../Components/Error';
 import Footer from '../Components/Footer';
 import Home from '../Components/Home';
 import Navbar from '../Components/Navbar';
@@ -10,26 +9,19 @@ import ProductItem from '../Components/ProductItem';
 
 const Routing = () => {
 
-  const [isAuthPage, setIsAuthPage] = useState(true);
 
   return (
     <Router>
-      {isAuthPage ?
-        <div className="container">
-          <Navbar />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            {/* <Route exact path="/about" component={About} /> */}
-            <Route exact path="/product" component={Product} />
-            <Route path="/product/:id" component={ProductItem} />
-          </Switch>
-          <Footer />
-        </div> :
+      <div className="container">
+        <Navbar />
         <Switch>
-          <Route path="*">
-            <Error setIsAuthPage={setIsAuthPage} />
-          </Route>
-        </Switch>}
+          <Route exact path="/" component={Home} />
+          {/* <Route exact path="/about" component={About} /> */}
+          <Route exact path="/product" component={Product} />
+          <Route path="/product/:id" component={ProductItem} />
+        </Switch>
+        <Footer />
+      </div>
     </Router>
   )
 }
